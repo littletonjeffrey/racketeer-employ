2f579c53dbc5b59f44154b26b776e186acdb8a4a
Jeffrey Littleton <littletonjeffrey@gmail.com>
2022-08-18 20:19:14 -0500

    changed versions on npm packages, created router in app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //Require Dependencies
 const express = require('express');
-const exhbs = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 
@@ -19,7 +19,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //app to parse json
 app.use(bodyParser.json());
 
+//static files
 app.use(express.static('public'));
+
+// Initializing handlebars and changing extension name to a shorter format
+app.engine('hbs', exphbs( {extname: '.hbs'}));
+
+app.set('view engine', 'hbs');
+
+//router
+app.get('/', (req, res) => {
+    res.render('index');
+});
 
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
