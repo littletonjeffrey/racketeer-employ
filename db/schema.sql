ebba40e2ca1cc1f6af80d06334187b2dd40e4d67
Jeffrey Littleton <littletonjeffrey@gmail.com>
2022-08-19 22:23:44 -0500

    set final schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,23 +1,43 @@
+-- deletes any old version of databas and creates a new one
 DROP DATABASE IF EXISTS sokovia_db;
 CREATE DATABASE sokovia_db;
-
+-- prepares tables to be inserted in above db
 USE sokovia_db;
-
-CREATE TABLE `metas` (
-  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `firstName` VARCHAR(45) NOT NULL, 
-  `lastName` VARCHAR(45) NOT NULL, 
-  `alias` VARCHAR(45) NOT NULL,
-  `species` VARCHAR(45) NOT NULL, 
-  `team` VARCHAR(45), 
-  `threatLevel` VARCHAR(45) NOT NULL, 
-);
- 
-CREATE TABLE 'knownTeams' (
-    'teamName' VARCHAR(45) NOT NULL,
-    'leader' VARCHAR(45),
-    'active' BINARY,
-    FOREIGN KEY (teamName)
-    REFERENCES metas(team)
-    ON DELETE SET NULL
+-- drops previous table if exists
+DROP TABLE IF EXISTS threat_level
+-- creates new table for threat levels
+CREATE TABLE threat_level(
+  id INT NOT NULL AUTO_INCREMENT,
+  threat_lev VARCHAR (30),
+  PRIMARY KEY (id)
 )
+-- drops previous table if exists
+DROP TABLE IF EXISTS known_teams
+-- creates new table for teams
+CREATE TABLE known_teams (
+  id INT NOT NULL AUTO_INCREMENT,
+  team_name VARCHAR (45),
+  leader VARCHAR (45),
+  PRIMARY KEY (id)
+)
+-- drops previous table if exists
+DROP TABLE IF EXISTS supes
+-- creates new table for people
+CREATE TABLE supes (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(45), 
+  last_name VARCHAR(45), 
+  alias VARCHAR(45),
+  team_id INT,
+  threat_level id INT
+  PRIMARY KEY (id)
+  -- allows this person to be assigned to team in known_teams table
+  FOREIGN KEY (team_id) REFERENCES known_teams(id)
+  --if the referenced team is deleted, set this person's team to null
+    ON DELETE SET NULL,
+  -- allows this person to be assigned a threat level in threat_levwel table
+  FOREIGN KEY (threat_level id) REFERENCES threat_level(id)
+);
+
+
+
---
+++ db/seeds.sql
@@ -0,0 +1,45 @@
+INSERT INTO peopleOfInterest (firstName, lastName, alias, species, threatLevel)
+VALUES ("James", "Howlett", "Wolverine", "mutant", "street"),
+       ("Scott", "Summers", "Cyclops", "mutant", "street"),
+       ("Jean", "Grey-Summers", "Marvel-Girl/Phoenix", "mutant", "galactic"),
+       ("Robert", "Drake", "Iceman", "mutant", "state"),
+       ("Henry", "McCoy", "Beast", "mutant", "street")
+       ("Charles", "Xavier", "Proffesor X", "global"),
+       ("Erik", "Lensherr", "Magneto", "global"),
+       ("Tony", "Stark", "Iron Man", "human", "state"),
+       ("Bruce", "Banner", "Hulk, The", "meta-human", "global"),
+       ("Steve", "Rogers", "Captain America", "meta-human", "street"),
+       ("Wanda", "Maximoff", "Scarlet Witch", "mutant", "universal"),
+       ("Thor", "Odinson", "", "Asgardian", "state"),
+       ("Vision", " ", " ", "Android", "global"),
+       ("Thanos", " ", "Mad Titan, The", "Titanian Eternal", "universal"),
+       ("Ultron", " ", " ", "Android", "global"),
+       ("Luther", "Hargreeves", "Number 1", "meta-human", "street"),
+       ("Diego", "Hargreeves", "Number 2", "meta-human", "street"),
+       ("Allison", "Hargreeves", "Number 3", "meta-human", "state"),
+       ("Klaus", "Hargreeves", "Number 4", "meta-human", "global"),
+       (" ", "Hargreeves", "Number 5", "meta-human", "universal"),
+       ("Ben", "Hargreeves", "Number 6", "meta-human", "deceased"),
+       ("Victor", "Hargreeves", "Number 7", "meta-human", "global"),
+       ("John", " ", "Homelander", "meta-human", "global"),
+       ("Maggie", "Shaw", "Queen Maeve", "meta-human", "state"),
+       ("Reggie", "Franklin", "A-Train", "meta-human", "state"),
+       ("Kevin", " ", "Deep, The", "meta-human", "street"),
+       (" ", " ", "Black Noir", "meta-human", "street"),
+       ("Annie", "January", "Starlight", "meta-human", "street"),
+       (" ", " ", "Stormfront", "meta-human", "state"),
+       ("Victor", "Hargreeves", "Father", "unknown", "state"),
+       ("Jonathan", "Osterman", "Dr. Manhattan", "meta-human", "cosmic")
+       ("Walter", "Kovacs", "Rorshach", "human", "street"),
+       ("Daniel", "Dreiberg", "Nite Owl II", "human", "street"),
+       ("Edward", "Blake", "Comedian, The", "human", "street"),
+       ("Adrian", "Veidt", "Ozymandias", "human", "state");
+
+
+
+
+
+       
+       
+
+
